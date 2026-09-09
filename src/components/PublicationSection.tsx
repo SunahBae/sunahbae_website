01267749ff8e664755e4b0563cbd6ab7e04e1752
SunahBae <sa.bae@kichikkochi.com>
2026-09-09 13:56:01 +0900

    .

--- src/components/PublicationSection.tsx
+++ src/components/PublicationSection.tsx
@@ -128,7 +128,6 @@ export const PublicationSection: React.FC = () => {
                   videoUrl={pub.video}
                   subline={highlightMe(pub.authors)}
                   thumbnails={pub.thumbnails}
-                  thumbnailFit="contain"
                 />
               );
             })}
